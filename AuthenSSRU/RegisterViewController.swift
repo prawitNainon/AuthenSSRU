8b3ac646c00edc4531ce90d523e1dc70346d6e79
masterKIE <prawit.na59@snru.ac.th>
2019-03-20 16:21:44 +0700

    Cloud Success

--- AuthenSSRU/RegisterViewController.swift
+++ AuthenSSRU/RegisterViewController.swift
@@ -19,6 +19,8 @@ class RegisterViewController: UIViewController {
     } //Main Method
     
     
+    @IBAction func uploadButton(_ sender: UIBarButtonItem) {
+    }
     
     @IBAction func BackButton(_ sender: UIBarButtonItem) {
         

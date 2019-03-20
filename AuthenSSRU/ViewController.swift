8ff14b875487d414518ea74f85dd82071fc3d58d
masterKIE <prawit.na59@snru.ac.th>
2019-03-20 14:08:42 +0700

    Get Event From Cllck Success

--- AuthenSSRU/ViewController.swift
+++ AuthenSSRU/ViewController.swift
@@ -11,14 +11,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+    
         // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    } //Main Metthod
+
+    @IBAction func rogisterButton(_ sender: UIButton) {
+        
+        print("You Click Register")
+        
     }
-
-
-}
+    
+} //Main Class
 
